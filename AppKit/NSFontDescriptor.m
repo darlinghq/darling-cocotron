b27543725de637c86513b3c4f754c837a41ebbc5
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-05-24 01:47:17 +0100

    missing symbols required by MorxTester
    fixes https://github.com/darlinghq/darling/issues/801

--- AppKit/NSFontDescriptor.m
+++ AppKit/NSFontDescriptor.m
@@ -41,6 +41,8 @@ NSString *const NSFontWeightTrait = @"NSFontWeightTrait";
 NSString *const NSFontWidthTrait = @"NSFontWidthTrait";
 NSString *const NSFontSlantTrait = @"NSFontSlantTrait";
 
+NSString *const NSFontFeatureSelectorIdentifierKey = @"NSFontFeatureSelectorIdentifier";
+
 const NSFontWeight NSFontWeightThin = 0xbfe3333340000000;
 const NSFontWeight NSFontWeightLight = 0xbfd99999a0000000;
 const NSFontWeight NSFontWeightUltraLight = 0xbfe99999a0000000;
